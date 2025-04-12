8619a28db1835e0b6a4a1a5e157cf7645e71e17f
EvelynYs2 <pewlye@gmail.com>
2025-04-12 09:56:23 -0300

    exercicio29

---
+++ src/Circulo.java
@@ -0,0 +1,25 @@
+package src;
+
+public class Circulo {
+    private double raio;
+
+public Circulo() {
+    this.raio = 0.0;
+}
+
+public Circulo(double raio) {
+    this.raio  = raio;
+}
+
+public double getRaio() {
+    return raio;
+}
+
+public void setRaio(double raio) {
+    this.raio = raio;
+}
+
+public double calcularArea() {
+    return Math.PI *raio*raio;
+  }
+}
